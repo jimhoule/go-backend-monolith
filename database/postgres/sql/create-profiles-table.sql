8236a09d18c800595e30e0e9847af2105a98863e
Jimmy Houle <jimhoule09@gmail.com>
2024-04-07 16:22:55 -0400

    create profiles table sql query adjusment

--- database/postgres/sql/create-profiles-table.sql
+++ database/postgres/sql/create-profiles-table.sql
@@ -3,7 +3,7 @@ CREATE TABLE profiles (
   name VARCHAR(50) NOT NULL,
   account_id UUID NOT NULL,
   language_id UUID,
-  CONSTRAINT pk_profile PRIMARY KEY(id)
+  CONSTRAINT pk_profile PRIMARY KEY(id),
   CONSTRAINT fk_account FOREIGN KEY(account_id) REFERENCES accounts(id) ON DELETE CASCADE,
   CONSTRAINT fk_language FOREIGN KEY(language_id) REFERENCES languages(id) ON DELETE SET NULL
 );
