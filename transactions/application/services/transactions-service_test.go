7741e5ea6d7e9c0415729fc17dedde9b3b87a939
Jimmy Houle <jimhoule09@gmail.com>
2024-04-09 17:16:42 -0400

    adding unit tests to Transactions Module

---
+++ transactions/application/services/transactions-service_test.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"app/transactions/persistence/fake/repositories"
+	"context"
+	"fmt"
+	"testing"
+)
+
+func getTestContext() (*TransactionsService) {
+	transactionsService := &TransactionsService{
+		TransactionsRepository: &repositories.FakeTransactionsRepository{},
+	}
+
+	return transactionsService
+}
+
+func TestTransactionsExecuteServiceSuccess(t *testing.T) {
+	transactionsService := getTestContext()
+
+	_, err := transactionsService.Execute(
+		context.Background(),
+		func(ctx context.Context) (any, error) {
+			return true, nil
+		},
+	)
+	if err != nil {
+		t.Errorf("Expected Transaction to be successfull but got %v", err)
+	}
+}
+
+func TestTransactionsExecuteServiceFailure(t *testing.T) {
+	transactionsService := getTestContext()
+
+	_, err := transactionsService.Execute(
+		context.Background(),
+		func(ctx context.Context) (any, error) {
+			return nil, fmt.Errorf("Failed to execute Transaction")
+		},
+	)
+	if err == nil {
+		t.Errorf("Expected Transaction to fail but got %v", err)
+	}
+}
